ce09ade5d2d7cb7fa8256259ac5f1853afa0254b
XLRC888 <181596240+XLRC888@users.noreply.github.com>
2026-06-21 17:37:35 +0300

    add zsh tab completions for all commands and subcommands

--- editerm.zsh
+++ editerm.zsh
@@ -2,3 +2,10 @@ editerm_prompt() {
     local s=$(editerm prompt 2>/dev/null)
     [[ -n "$s" ]] && print -n " ${s}"
 }
+
+edir=${0:A:h}
+if [[ -z ${fpath[(r)$edir]} ]]; then
+    fpath=($edir $fpath)
+fi
+autoload -Uz _editerm
+unset edir
